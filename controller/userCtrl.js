7b92e3d98abec9c3e97bfa583b9b4cbefba1f08d
muhammadrizalsupriadi <muhammadrizalsupriadi@gmail.com>
2023-10-17 09:53:41 +0700

    Login User and JWT

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const generateToken = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require("express-async-handler")
 
@@ -13,5 +14,21 @@ const createUser = asyncHandler(async (req, res) => {
     };
 });
 
+const loginUserCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const findUser = await User.findOne({ email });
+    if(findUser && (await findUser.isPasswordMatched(password))){
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
+    }else{
+        throw new Error("Invalid Creadentials");
+    }
+})
 
-module.exports=createUser;
+module.exports = {createUser, loginUserCtrl};
